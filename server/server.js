8044e658b2a9c0209b4f192303e897a512be8ce1
Hafilue c <123727321+Hafilu@users.noreply.github.com>
2024-01-21 17:35:21 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import path from 'path';
 import { config } from 'dotenv';
 import router from './router/router.js';
  
